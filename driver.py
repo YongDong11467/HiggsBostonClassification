164bc27a03cea06a1ea6c00cf6355dd71c1729ba
YongDong11467 <yongdong11467@gmail.com>
2021-04-25 16:01:30 -0400

    added graphs

--- driver.py
+++ driver.py
@@ -2,10 +2,18 @@ import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 import nearestneigbor
 
 rawData = pd.read_csv (r'./higgs-boson/training.csv')
 # rawtest = pd.read_csv (r'./higgs-boson/test.csv')
+
+# TP, TN, FP, FN
+C_roc = []
+
+# TP, TN, FP, FN
+N_roc = []
 
 def processData():
     data = rawData[:1000]
@@ -22,47 +30,32 @@ def processData():
     validationData = validationData.iloc[:, :-1]
     return trainData.values, testData.values, validationData.values, trainLabel.values, testLabel.values, validationLabel.values
 
-# def kfoldcrossvalid(B,X_subset,y_subset,C):
-#     n = len(X_subset)
-#     bs_err = np.zeros(B)
-#     for b in range(B):
-#         train_samples = list(np.random.randint(0,n,n))
-#         test_samples = list(set(range(n)) - set(train_samples))
-#         alg = SVC(C=C,kernel='linear')
-#         alg.fit(X_subset[train_samples], y_subset[train_samples])
-#         bs_err[b] = np.mean(y_subset[test_samples] != alg.predict(X_subset[test_samples]))
-#     err = np.mean(bs_err)
-#     return err
-
-
 def kfoldcrossvalid(k, X, y, C, nFlag):
     (n, d) = np.shape(X)
     kf_err = np.zeros(k)
+    if nFlag:
+        alg = KNeighborsClassifier(n_neighbors=C)
+    else:
+        alg = SVC(C=C, kernel='linear')
+
     for i in range(0, k):
         T = set(range(int(np.floor((n * i) / k)), int(np.floor(((n * (i + 1)) / k) - 1)) + 1))
         S = set(range(0, n)) - T
 
-        if nFlag:
-            alg = KNeighborsClassifier(n_neighbors=C)
-        else:
-            alg = SVC(C=C, kernel='linear')
         alg.fit(X[list(S)], y[list(S)])
         kf_err[i] = np.mean(y[list(T)] != alg.predict(X[list(T)]))
+    CM = confusion_matrix(y, alg.predict(X))
+
+    TN = CM[0][0]
+    FN = CM[1][0]
+    TP = CM[1][1]
+    FP = CM[0][1]
+    if nFlag:
+        N_roc.append([TP, TN, FP, FN])
+    else:
+        C_roc.append([TP, TN, FP, FN])
     err = np.mean(kf_err)
     return err
-
-# def kfoldcrossvalid(k, X, y, C):
-#     (n, d) = np.shape(X)
-#     kf_err = np.zeros(k)
-#     for i in range(0, k):
-#         T = set(range(int(np.floor((n * i) / k)), int(np.floor(((n * (i + 1)) / k) - 1)) + 1))
-#         S = set(range(0, n)) - T
-#
-#         alg = KNeighborsClassifier(n_neighbors=C)
-#         alg.fit(X[list(S)], y[list(S)])
-#         kf_err[i] = np.mean(y[list(T)] != alg.predict(X[list(T)]))
-#     err = np.mean(kf_err)
-#     return err
 
 def main():
     trainData, testData, validationData, trainLabel, testLabel, validationLabel = processData()
@@ -75,13 +68,18 @@ def main():
     # print(acc / 1000)
 
     C_list = [0.1, 0.3, 0.5, 0.7]
+    C_list_acc = []
+
     N_list = [1, 3, 5, 7]
+    N_list_acc = []
+
     k = 10
     best_C = 0.0
     best_err = 1.1
     trainLabel = np.ravel(trainLabel.reshape(-1))
     for C in C_list:
         err = kfoldcrossvalid(k, trainData, trainLabel, C, nFlag=False)
+        C_list_acc.append(1 - err)
         print("C=", C, ", err=", err)
         if err < best_err:
             best_err = err
@@ -92,12 +90,56 @@ def main():
     best_err = 1.1
     for N in N_list:
         err = kfoldcrossvalid(k, trainData, trainLabel, N, nFlag=True)
+        N_list_acc.append(1 - err)
         print("N=", N, ", err=", err)
         if err < best_err:
             best_err = err
             best_N = N
     print("best_N=", best_N)
     print("Finish Preprocessing")
+
+    # plt.plot(C_list, C_list_acc, label='Hyper_Accuracy_Plot(C)')
+    # plt.xlabel('Hyperparameter C')
+    # plt.ylabel("accuracy (%)")
+    # plt.axis([0, .8, .6, .8])
+    # plt.savefig("Hyper_Accuracy_Plot(C).pdf")
+    # plt.show()
+    #
+    # plt.plot(N_list, N_list_acc, label='Hyper_Accuracy_Plot(N)')
+    # plt.xlabel('Hyperparameter N')
+    # plt.ylabel("accuracy (%)")
+    # plt.axis([0, 8, .6, .8])
+    # plt.savefig("Hyper_Accuracy_Plot(N).pdf")
+    # plt.show()
+
+    print(C_roc)
+    print(N_roc)
+
+    x = [0, 0, 0, 0]
+    y = [0, 0, 0, 0]
+    for i in range(0, 4):
+        d = C_roc[i]
+        x[i] = d[1]/(d[1] + d[2])
+        y[i] = d[0]/(d[0] + d[3])
+    plt.plot(x, y, label='ROC_Plot(C)')
+    plt.xlabel('Specificity')
+    plt.ylabel("Sensitivity")
+    plt.axis([0, 1, 0, 1])
+    plt.savefig("ROC_Plot(C).pdf")
+    plt.show()
+
+    x = [0, 0, 0, 0]
+    y = [0, 0, 0, 0]
+    for i in range(0, 4):
+        d = N_roc[i]
+        x[i] = d[1]/(d[1] + d[2])
+        y[i] = d[0]/(d[0] + d[3])
+    plt.plot(x, y, label='ROC_Plot(N)')
+    plt.xlabel('Specificity')
+    plt.ylabel("Sensitivity")
+    plt.axis([0, 1, 0, 1])
+    plt.savefig("ROC_Plot(N).pdf")
+    plt.show()
 
 
 main()
